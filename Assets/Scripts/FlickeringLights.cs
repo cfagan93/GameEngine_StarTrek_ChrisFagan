6ec80993c4687bdd22e12e465b652b4ba4d7930f
cfagan93 <44029422+cfagan93@users.noreply.github.com>
2020-05-18 13:45:08 +0100

    Scripts

---
+++ Assets/Scripts/FlickeringLights.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLights : MonoBehaviour
+{
+    private Light lights;
+   
+    void Start()
+    {
+        lights = this.gameObject.GetComponent<Light>();
+        StartCoroutine("flicker");  
+    }
+
+    IEnumerator flicker ()
+    {
+        while (true)
+        {
+            lights.enabled = false;
+            float wait = Random.Range(0.4f, 0.9f);
+            yield return new WaitForSeconds(wait);
+            lights.enabled = true;
+            wait = Random.Range(0.4f, 0.9f);
+            yield return new WaitForSeconds(wait);
+        }
+    }
+}
