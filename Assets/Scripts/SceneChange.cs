487e0b0a4c2ea0c26aecbc93fdd283e26df1bee8
cfagan93 <44029422+cfagan93@users.noreply.github.com>
2020-05-18 18:12:37 +0100

    Update

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+
+
+    public int num;
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(num);
+        Debug.Log("Scene changed to " + num);
+
+    }
+
+}
+
